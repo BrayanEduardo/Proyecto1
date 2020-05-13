abd88aae8e102829a1afaa8fd523d56741139363
BrayanEduardo <bf377954@gmail.com>
2020-05-12 20:18:55 -0600

    nombre Brayan

--- src/proyecto1/Proyecto1.java
+++ src/proyecto1/Proyecto1.java
@@ -18,6 +18,7 @@ public class Proyecto1 {
         // TODO code application logic here
         System.out.println("Hola Mundo");
          System.out.println("Hola Mundo denuevo");
+         System.out.println("Brayan Eduardo");
          } 
     
 }
